9f18476fa7fda6de2780b67fbe1548eb1c632142
Vincent Andresson <Nqress@gmail.com>
2026-02-06 17:12:52 +0700

    week01: (tugas 2) setup GameStore file

---
+++ week01/GameStore.kt
@@ -0,0 +1,12 @@
+package oop_110239_VincentAndreeson.week01
+
+// Diskon : 20% jika harga > 50.000, sisanya 10%
+// Output : Tampilkan Judul, Harga Asli, Harga Akhir
+// Wajib : Terapkan semua konsep 'Clean Code' yang sudah dipelajari (val, expression, logic, named arguments)
+
+fun main() {
+    var price : Int = 890000
+    val gameTitle = "BuburLegend"
+
+}
+
