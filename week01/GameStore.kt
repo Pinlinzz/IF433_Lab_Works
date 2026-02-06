7ac4376bdc27a710eede6cd18995118574f26ee5
Vincent Andresson <Nqress@gmail.com>
2026-02-06 17:47:09 +0700

    week01: (tugas 2) implement discount logic

--- week01/GameStore.kt
+++ week01/GameStore.kt
@@ -5,8 +5,14 @@ package oop_110239_VincentAndreeson.week01
 // Wajib : Terapkan semua konsep 'Clean Code' yang sudah dipelajari (val, expression, logic, named arguments)
 
 fun main() {
-    var price : Int = 890000
+    val price : Int = 890000
     val gameTitle = "BuburLegend"
 
+    calculateDiscounted(price)
+}
+
+fun calculateDiscounted(price: Int): Double = when {
+    price > 50000 -> price * 0.8 // Diskon 20%, maka bayar 80%
+    else -> price * 0.9          // Diskon 10%, maka bayar 90%
 }
 
