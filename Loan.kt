51a58ec435e0e8a57cf71a95fedda223daa579b8
unknown <Nqress@gmail.com>
2026-02-13 11:41:31 +0700

    week02: (task 1) create Loan class with default args

---
+++ Loan.kt
@@ -0,0 +1,27 @@
+package oop_110239_VincentAndresson.week02
+
+import kotlin.time.Duration
+
+class Loan (
+    val bookTitle : String,
+    var borrower : String,
+    var loanDuration: Int = 1 //default nya 1 hari
+)
+{
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) { // kalo pinjem lebih dari 3 hari bayar denda
+            (loanDuration - 3) * 2000 // denda 2rb per hari
+        } else {
+            0
+        }
+    }
+
+    fun displayBuku() {
+        println("\n--- Detail Peminjaman ---")
+        println("Judul Buku    : $bookTitle")
+        println("Peminjam      : $borrower")
+        println("Durasi Pinjam : $loanDuration hari")
+        println("Total Denda   : Rp ${calculateFine()}")
+        println("-------------------------")
+    }
+}
