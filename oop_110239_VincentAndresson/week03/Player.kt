1a1689f8ed913f3f02c3333c24d95f4dab78c3cf
unknown <Nqress@gmail.com>
2026-02-20 17:12:46 +0700

    week03: (task 2) create Player class with private xp

---
+++ oop_110239_VincentAndresson/week03/Player.kt
@@ -0,0 +1,30 @@
+package oop_110239_VincentAndresson.week03
+
+class Player(val username: String) {
+    private var xp: Int = 0 // diset ke private agar ga nyasar keluar
+
+    val level: Int // ini pake rumus yang ada di modul
+        get () = (xp / 100 ) + 1 // (Contoh: XP 0-99 = Level 1, XP 100 = Level 2).
+
+    fun addXp(ammount : Int) {
+        if (ammount <= 0) {
+            println("Jumlah Xp harus lebih dari 0! 🫵😈")
+            return
+        }
+
+        val levelSebelumnya = level //nyimpen level lama buat ngetriger levelUp
+
+        xp += ammount
+        if(level > levelSebelumnya) {
+            println("Level Up! selamat $username naik ke level $level")
+        }
+    }
+
+    fun displayStatus(){
+        println("Player: $username" +
+                "Xp: $xp" +
+                "Level: $level"
+         )
+    }
+
+}
---
+++ oop_110239_VincentAndresson/week03/mainWeapon.kt
@@ -0,0 +1,27 @@
+package oop_110239_VincentAndresson.week03
+
+fun main() {
+
+    val myWeapon = Weapon("Puding Coklat Pak Hambali 🙀", 30)
+    println("Nama: ${myWeapon.name} Damage: ${myWeapon.damage}")
+    println("----------------------------------------------")
+
+    // 1. Coba set damage ke -50 (Harus Gagal)
+    println("Mencoba set damage ke -50...")
+    myWeapon.damage = -50
+    println("Damage saat ini: ${myWeapon.damage}") // Nilai harus tetap 100
+    println("----------------------------------------------")
+
+    // 2. Coba set damage ke 9999 (Harus dipaksa jadi 1000)
+    println("Mencoba set damage ke 9999...")
+    myWeapon.damage = 9999
+    println("Damage saat ini: ${myWeapon.damage}") // Nilai harus menjadi 1000
+    println("----------------------------------------------")
+
+    // 3. Print Tier-nya
+    // Karena damage sekarang 1000 (> 800), tier harus "Legendary"
+    println("Hasil Akhir:")
+    println("Nama Senjata : ${myWeapon.name}")
+    println("Final Damage : ${myWeapon.damage}")
+    println("Tier Senjata : ${myWeapon.tier}")
+}
