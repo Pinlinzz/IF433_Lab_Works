9e5997c8e2814a4b12680fd877a2a25013b779d5
unknown <Nqress@gmail.com>
2026-02-20 11:03:01 +0700

    ngebenerin susunan folder

---
+++ oop_110239_VincentAndresson/week01/GameStore.kt
@@ -0,0 +1,33 @@
+package oop_110239_VincentAndresson.week01
+
+// Diskon : 20% jika harga > 50.000, sisanya 10%
+// Output : Tampilkan Judul, Harga Asli, Harga Akhir
+// Wajib : Terapkan semua konsep 'Clean Code' yang sudah dipelajari (val, expression, logic, named arguments)
+
+fun main() {
+    val price : Int = 890000
+    val gameTitle = "BuburLegend"
+
+    val receipt: String? = null
+
+    calculateDiscounted(price)
+    printReceipt(
+        title = gameTitle,
+        price = price,
+        finalPrice = calculateDiscounted(price),
+        note = receipt
+    )
+}
+
+fun calculateDiscounted(price: Int): Double = when {
+    price > 50000 -> price * 0.8 // Diskon 20%, maka bayar 80%
+    else -> price * 0.9          // Diskon 10%, maka bayar 90%
+}
+
+fun printReceipt(title: String, price: Int, finalPrice: Double, note: String?) {
+    println("=== Invoice Game ===")
+    println("Judul        : $title")
+    println("Harga Asli   : Rp$price")
+    println("Harga Akhir  : Rp$finalPrice")
+    println("Catatan      : ${note ?: "Tidak ada catatan"}")
+}
---
+++ oop_110239_VincentAndresson/week01/GradingSystem.kt
@@ -0,0 +1,32 @@
+package oop_110239_VincentAndresson.week01
+
+fun main () {
+    //REFATOR: GUNAKAN Val dan hapus tipe data eksplisit
+    var name = "John Thor"
+    var score = 80
+
+    // Concatenation  gaya lama
+    //println("Nama: " + name + ", Nilai: " + score)
+
+    // REFACTOR : String Template ($name)
+    println("Nama: $name, Nilai: $score")
+
+    val grade = when (score) {
+        in 90.. 100 -> "A"
+        in 80.. 89 -> "B"
+        in 70.. 79 -> "C"
+        else -> "D"
+    }
+    println("Grade Kau : $grade")
+    println("Status: ${calcultedStatus(score)}")
+
+    // Tambah di akhir main()
+    val studentId : String? = null
+    // Jika null, gunakan nili defailt 0
+    val idLength = studentId?.length ?: 0
+    println("Panjang Id: $idLength")
+}   
+
+// Tulis DI LUAR main()
+fun calcultedStatus(score : Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
