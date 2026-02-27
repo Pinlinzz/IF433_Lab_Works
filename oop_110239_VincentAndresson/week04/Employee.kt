677bf573147fdbaac8cb42537cd11e128d5d37a2
unknown <Nqress@gmail.com>
2026-02-27 11:05:20 +0700

    week04: (task 2) create parent class Employee

---
+++ oop_110239_VincentAndresson/week04/Employee.kt
@@ -0,0 +1,14 @@
+package oop_110239_VincentAndresson.week04
+
+//'open' dipake agar class ini bisa diwariskan ke posisi seperti Manager atau Developer
+open class Employee(val name: String, val baseSalary: Int) {
+
+    open fun work() { // work bisa diwarisin
+        println("$name sedang bekerja.")
+    }
+
+    // Method untuk menghitung bonus 10% dari baseSalary
+    open fun calculateBonus(): Int {
+        return (baseSalary * 0.1).toInt(
+    }
+}
