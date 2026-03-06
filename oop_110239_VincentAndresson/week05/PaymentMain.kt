61bf856dfcc785fa655c9077fca0d72049e447c5
unknown <Nqress@gmail.com>
2026-03-06 11:46:13 +0700

    week05: (task 2) implement payment polymorphism

---
+++ oop_110239_VincentAndresson/week05/PaymentMain.kt
@@ -0,0 +1,21 @@
+package oop_110239_VincentAndresson.week05
+
+fun main() {
+    // Inisiasi objek
+    val myWallet = EWallet("Budi", 50000.0)
+    val myCard = CreditCard("Budi", 100000.0)
+
+    // Masukin ke dalem list PaymentMethod (Polymorphism)
+    val payments: List<PaymentMethod> = listOf(myWallet, myCard)
+
+    val tagihan = 75000.0
+
+    for (payment in payments) {
+        println("--- Memproses pembayaran untuk: ${payment.accountName} ---")
+
+        // Testing payment pertama
+        payment.processPayment(tagihan)
+
+        println()
+    }
+}
