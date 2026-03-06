5574125d5df52db80ce6868b6255c464ffa01cb4
unknown <Nqress@gmail.com>
2026-03-06 10:17:44 +0700

    week05: create abstract class Pegawai

---
+++ oop_110239_VincentAndresson/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_110239_VincentAndresson.week05
+
+abstract class Pegawai(val nama: String) {
+    // Bastract functiion tidak memiliki body(tanpa kurung kurawal)
+    abstract fun bekerja()
+}
