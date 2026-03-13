907f5dd3f4c45eb9d1f2f37bb708ae27b84d8606
unknown <Nqress@gmail.com>
2026-03-13 09:58:45 +0700

    week06: fix Clickable interface using abstract property

--- oop_110239_VincentAndresson/week6/Clickable.kt
+++ oop_110239_VincentAndresson/week6/Clickable.kt
@@ -1,9 +1,6 @@
 package oop_110239_VincentAndresson.week6
 
 interface Clickable {
-    // ERROR : Property in an interacen cannot have a backing field
-    val name: String = "Tombol Rahasia"
-
-    // funct without body (Impicity Abstract)
+    val name : String // Abstrack property, tidak ada nilai default
     fun click()
 }
