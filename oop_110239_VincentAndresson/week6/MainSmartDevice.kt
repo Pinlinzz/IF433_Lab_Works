4433ba5a0023b2a556b097411f001a51e31f5a11
unknown <Nqress@gmail.com>
2026-03-13 11:09:12 +0700

    week06: (task) test smart home system logic in main

--- oop_110239_VincentAndresson/week6/MainSmartDevice.kt
+++ oop_110239_VincentAndresson/week6/MainSmartDevice.kt
@@ -7,4 +7,18 @@ fun main() {
     val speakerDapur = SmartSpeaker("S-01", "Google Nest Dapur")
     val cctvGarasi = SmartCCTV("C-01", "Ezviz Garasi")
 
+    val hub = SmartHomeHub()
+
+    println("=== Inisialisasi Sistem Smart Home ===")
+    // iniasiiin semua device
+    hub.addDevice(lampuTamu)
+    hub.addDevice(speakerDapur)
+    hub.addDevice(cctvGarasi)
+
+    //nyalain camera + speker buat sirine
+    hub.activateSecurityMode()
+
+    hub.turnOffAllSwitches() // matiin semua device
+
+    println("\n=== Selesai: Semua sistem telah diproses ===")
 }
