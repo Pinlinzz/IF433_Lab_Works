44e4357a6b312558ae40a61585217123848fb24b
unknown <Nqress@gmail.com>
2026-03-13 10:17:58 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ oop_110239_VincentAndresson/week6/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_110239_VincentAndresson.week6
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif")}
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan")}
+}
