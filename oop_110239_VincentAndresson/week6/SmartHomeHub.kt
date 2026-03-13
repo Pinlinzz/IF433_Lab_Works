164cf5432af6c1bd8b77dfa25e27f520b31c9e47
unknown <Nqress@gmail.com>
2026-03-13 11:03:23 +0700

    week06: (task) create SmartHomeHub logic

---
+++ oop_110239_VincentAndresson/week6/SmartHomeHub.kt
@@ -0,0 +1,19 @@
+package oop_110239_VincentAndresson.week6
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("Berhasil menambahkan: ${device.name} (ID: ${device.id})")
+    }
+
+    fun turnOffAllSwitches() {
+        println("\n--- Mematikan Semua Saklar ---")
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+}
