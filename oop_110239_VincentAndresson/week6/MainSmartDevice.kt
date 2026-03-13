8a6069bc28581b2a737c65d5df1f9756a795019f
unknown <Nqress@gmail.com>
2026-03-13 11:07:25 +0700

    week06: (task) instantiate smart devices in main

---
+++ oop_110239_VincentAndresson/week6/MainSmartDevice.kt
@@ -0,0 +1,10 @@
+package oop_110239_VincentAndresson.week6
+
+// Main.kt
+
+fun main() {
+    val lampuTamu = SmartLamp("L-01", "Ruang Tamu")
+    val speakerDapur = SmartSpeaker("S-01", "Google Nest Dapur")
+    val cctvGarasi = SmartCCTV("C-01", "Ezviz Garasi")
+
+}
