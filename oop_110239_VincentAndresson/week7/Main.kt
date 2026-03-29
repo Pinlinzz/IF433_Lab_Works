5add20611f828f7df664d3ec1b4c122039f97590
unknown <Nqress@gmail.com>
2026-03-29 19:12:23 +0700

    week07: test Singleton and Companion Object in main

--- oop_110239_VincentAndresson/week7/Main.kt
+++ oop_110239_VincentAndresson/week7/Main.kt
@@ -1,6 +1,14 @@
 package oop_110239_VincentAndresson.week7
 
 fun main() {
-    val client = NetworkClient("https://api.umn.ac.id") // ini error karena constructornya masih private
+    //val client = NetworkClient("https://api.umn.ac.id") // ini error karena constructornya masih private
 
+
+    println("=== TEST SINGLETON ===")
+    println("Status ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient() //Instansiasi lewat factor
+    client.connect()
 }
