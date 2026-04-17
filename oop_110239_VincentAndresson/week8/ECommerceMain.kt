49be086c7c2dbc400c5c040a92a7c4e9f09b5c75
unknown <Nqress@gmail.com>
2026-04-17 11:13:20 +0700

    week08: (task) process mixed raw JSON list using as?

--- oop_110239_VincentAndresson/week8/ECommerceMain.kt
+++ oop_110239_VincentAndresson/week8/ECommerceMain.kt
@@ -3,8 +3,15 @@ package oop_110239_VincentAndresson.week8
 // week08: (task) implement payment processing with !!
 // week08: (task) batch processing in main
 fun main() {
-   val parser = ApiParser()
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"), // corrupted
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // missing ID
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // unknown type
+    )
 
+   val parser = ApiParser()
     for (raw in rawApiData) {
         try {
             val product = parser.parseProduct(raw)
