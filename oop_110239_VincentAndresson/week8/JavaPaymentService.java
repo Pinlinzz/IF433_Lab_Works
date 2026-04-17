6e82da4d6d8c55ee9b71e86497602ad974cb5d13
unknown <Nqress@gmail.com>
2026-04-17 11:09:39 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ oop_110239_VincentAndresson/week8/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_110239_VincentAndresson.week8;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
+
