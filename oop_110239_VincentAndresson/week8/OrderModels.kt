4609fe47bf973f77cb55d788223fb51c55afa065
unknown <Nqress@gmail.com>
2026-04-17 10:20:58 +0700

    week08: create nested classes for Order

---
+++ oop_110239_VincentAndresson/week8/OrderModels.kt
@@ -0,0 +1,6 @@
+package oop_110239_VincentAndresson.week8
+
+class City(val name : String)
+class Address(val city : City?)
+class DeliveryDetails(val address : Address?)
+class Order (val deliveryDetails : DeliveryDetails?, val totalPrice : Int?)
