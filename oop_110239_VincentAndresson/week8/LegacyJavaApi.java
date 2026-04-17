5ad7cbe1a371372e1a8dc4ff2436de025877c562
unknown <Nqress@gmail.com>
2026-04-17 10:47:06 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ oop_110239_VincentAndresson/week8/LegacyJavaApi.java
@@ -0,0 +1,8 @@
+package oop_110239_VincentAndresson.week8;
+
+public class LegacyJavaApi {
+    // FUngsi ini ditulis dalam Java. Kotilin tidak tahu apalklah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
