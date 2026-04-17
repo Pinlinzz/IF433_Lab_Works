a6f73e2aa8ae1f92f88e0642efe27d52dea1967b
unknown <Nqress@gmail.com>
2026-04-17 11:07:05 +0700

    week08: (task) create sealed class Product and data classes

---
+++ oop_110239_VincentAndresson/week8/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_110239_VincentAndresson.week8
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
