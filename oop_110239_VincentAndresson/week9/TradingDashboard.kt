2b1daad19194ffc9062d04f62ded7dbd3368472c
unknown <Nqress@gmail.com>
2026-04-27 21:31:16 +0700

    week09: (task) populate mock trade history list

---
+++ oop_110239_VincentAndresson/week9/TradingDashboard.kt
@@ -0,0 +1,15 @@
+package oop_110239_VincentAndresson.week9
+
+fun main () {
+    fun main() {
+        // --- Langkah 2: Inisialisasi Data Uji (Checkpoint 11) ---
+        // Syarat: Minimal 6 data, campuran OPEN/CLOSED, ROE positif/negatif, minimal 2 koin berbeda.
+        val tradeHistory = listOf(
+            TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+            TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+            TradeLog("BTCUSDT", "SHORT", 50, 45.0, "CLOSED"),
+            TradeLog("SOLUSDT", "LONG", 5, -2.5, "OPEN"),
+            TradeLog("ETHUSDT", "LONG", 15, 12.0, "OPEN"),
+            TradeLog("DOGEUSDT", "LONG", 10, -10.0, "CLOSED")
+        )
+}
