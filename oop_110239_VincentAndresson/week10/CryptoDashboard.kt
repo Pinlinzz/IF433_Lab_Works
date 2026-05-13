6731d5a8266e654299fff26064fa45960d7e97ea
unknown <Nqress@gmail.com>
2026-05-14 05:38:59 +0700

    week10: (task) populate coin repository

--- oop_110239_VincentAndresson/week10/CryptoDashboard.kt
+++ oop_110239_VincentAndresson/week10/CryptoDashboard.kt
@@ -3,4 +3,13 @@ package oop_110239_VincentAndresson.week10
 fun main() {
     println("=== DASHBOARD CRYPTO WALLET ===")
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.05))
+    coinRepo.add(Coin("ETH", 1.2))
+    coinRepo.add(Coin("USDT", 500.0))
+
+    // Uji coba fungsi pencarian dari Checkpoint 14
+    println("\n[UJI PENCARIAN]")
+    val searchResult = coinRepo.searchByName("ETH")
+    println("Hasil cari 'ETH': $searchResult")
 }
