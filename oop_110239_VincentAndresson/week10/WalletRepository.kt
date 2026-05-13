0bd53f9f49b3a932b1ba2e62faf674cc8dd92762
unknown <Nqress@gmail.com>
2026-05-14 05:35:00 +0700

    week10: (task) implement generic WalletRepository

---
+++ oop_110239_VincentAndresson/week10/WalletRepository.kt
@@ -0,0 +1,14 @@
+package oop_110239_VincentAndresson.week10
+
+class WalletRepository<T : Any> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+
+}
