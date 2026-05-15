33144e9f65dc4de8aef1ff1a172817f00b580b27
unknown <Nqress@gmail.com>
2026-05-15 14:05:18 +0700

    week12: (task) handle feeding execution with multiple catch

--- oop_110239_VincentAndresson/week12/SmartFeeder.kt
+++ oop_110239_VincentAndresson/week12/SmartFeeder.kt
@@ -1,5 +1,7 @@
 package oop_110239_VincentAndresson.week12
 
+import jdk.swing.interop.DispatcherWrapper
+
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
 
@@ -19,6 +21,21 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main () {
     var currentKibbleStock = 50
+
+    println("=== JADWAL MAKAN PAGI ===")
+    try {
+        dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+    } catch (e: DispenserJamException){
+        println("Error Mesin: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Error Stock: ${e.message}")
+    } catch (e: Exception) {
+        println("Error Umum: ${e.message}")
+    }
 }
 
 
